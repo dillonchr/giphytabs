579accf60978eae31cc05eaf4dfd739523567a9d
Dillon Christensen <j.dillon.christensen@gmail.com>
2017-12-02 20:47:25 -0600

    finally updated, just a dev key for now, cross browser requests broken

--- README.md
+++ README.md
@@ -2,6 +2,4 @@
 [GiphyTabs is a chrome extension](https://chrome.google.com/webstore/detail/giphy-tabs/dkajnomnlpcpkekglpbbhcmmgljadopo?hl=en-US) that replaces your new tab with random giphys. This is just some light modifications to that extension for now.
 
 ## TODO
-* Make icons so I'm not stealing
-* Get my own API key
 * Release!
--- tab.js
+++ tab.js
@@ -1,19 +1,17 @@
-((timeOut) => {
-    const WAIT = timeOut * 1000;
+((secondsToWait) => {
+    const WAIT = secondsToWait * 1000;
     const GIF = document.getElementById('gif');
     let wantsLandscape = window.innerWidth >= window.innerHeight;
-    let firstGif = true;
 
-    function resetImage() {
-        fetch('http://tv.giphy.com/v1/gifs/screensaver?api_key=wBL1FvTTel6OQ&tag=giphytrending&rating=pg')
+    function resetImage(firstGif) {
+        fetch('http://tv.giphy.com/v1/gifs/screensaver?api_key=2mbd9rzNUxHGhGcdmJ7LroifN007msvy&tag=giphytrending&rating=pg')
             .then(r => r.json())
             .then(({data: d}) => {
-                if (d.image_width >= d.image_height === wantsLandscape) {
+                if (d.image_width > d.image_height === wantsLandscape) {
                     if (firstGif) {
-                      firstGif = false;
                       updateBackground(d.image_url);
                     } else {
-                      preloadGif(d.image_url);
+                      preloadGif(d.image_url, d.image_width, d.image_height);
                     }
                 } else {
                     resetImage();
@@ -21,6 +19,7 @@
             });
     }
 
+    //  preload gif to prevent jankiness in animations
     function preloadGif(url, width, height) {
       const img = new Image(width, height);
       img.onload = () => updateBackground(url);
@@ -28,12 +27,18 @@
       img.src = url;
     }
 
+    //  changes tab background to show current gif
     function updateBackground(url) {
         GIF.style.backgroundImage = `url("${url}")`;
         setTimeout(resetImage, WAIT);
     }
 
+    //  if you wind up changing your window we can start
+    //  looking for appropriately oriented giphys
+    //  fair warning: not many portrait or square ones exist
     window.onresize = () => wantsLandscape = window.innerWidth >= window.innerHeight;
-
-    resetImage();
+    
+    //  set background without preload for first image
+    //  kick off loop
+    resetImage(true);
 })(5);
